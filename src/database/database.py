fac197958e81a739656a21054d9ce4681a2156a2
oktoberlin <oktoberlin@gmail.com>
2023-09-21 16:10:56 +0700

    clean code

--- src/database/database.py
+++ src/database/database.py
@@ -1,11 +1,11 @@
 import pymysql
 
 # database information
-user_db = 'jsms'
-pass_db = 'JSMSpass'
-host_db = '149.129.233.76'
-port_db = 3307
-name_db = 'mydepojavapjn'
+user_db = ''
+pass_db = ''
+host_db = ''
+port_db = 3306
+name_db = ''
 
 # connecting server database Jakarta to pythons
 db = pymysql.connect(user=user_db,password=pass_db,host=host_db,port=port_db,database=name_db)
